7484e4571c0538bb3900c9b37a1c5c61610fca93
Mykola Marusenko <marusenkonik@gmail.com>
2025-07-01 17:09:02 +0300

    Update dag_a.py

--- dag_a.py
+++ dag_a.py
@@ -1,4 +1,3 @@
-# dag_a.py
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
@@ -7,8 +6,14 @@ def task_a(**kwargs):
     print("A complete!")
     kwargs["ti"].xcom_push(key="result_a", value="result-from-a")
 
-with DAG(dag_id="dag_a", start_date=datetime(2023, 1, 1), schedule=None, catchup=False) as dag:
-run_a = PythonOperator(
-    task_id="run_a",
-    python_callable=task_a
-)
+with DAG(
+    dag_id="dag_a",
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    run_a = PythonOperator(
+        task_id="run_a",
+        python_callable=task_a,
+    )
