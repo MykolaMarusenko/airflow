e89d0c2b34581e42b1577148b75973c834276262
Mykola Marusenko <marusenkonik@gmail.com>
2025-07-01 16:26:38 +0300

    Update hello_k8s.py

--- hello_k8s.py
+++ hello_k8s.py
@@ -4,8 +4,8 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 
 with DAG(
     dag_id="hello_k8s",
-    schedule=None,  # вместо schedule_interval
-    start_date=datetime(2023, 1, 1),  # фиксированная дата
+    schedule=None,
+    start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=["example"],
 ) as dag:
