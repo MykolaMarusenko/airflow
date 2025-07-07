03d901e0193b2c8b76b7da319e299498419527dd
Mykola Marusenko <marusenkonik@gmail.com>
2025-07-07 20:56:40 +0300

    Update hello_k8s.py

--- hello_k8s.py
+++ hello_k8s.py
@@ -17,7 +17,4 @@ with DAG(
         cmds=["sh", "-c", "sleep 30"],
         is_delete_operator_pod=False,
         kubernetes_conn_id="mm-test",
-        in_cluster=False,
-        service_account_name="mm-airflow",
-        namespace="krci-bff-mm-dev",
     )
